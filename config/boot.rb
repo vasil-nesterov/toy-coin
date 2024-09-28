1b36c30c6d9f8b5609af9dd37281d802619bbf23
Vasil Nesterov <vasil@hey.com>
2024-09-28 11:45:07 +0200

    boot.rb: Add hot reloading

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,11 @@
 require "zeitwerk"
 require_relative "../config/boot"
 
-loader = Zeitwerk::Loader.new
-loader.push_dir("src")
-loader.setup
+$loader = Zeitwerk::Loader.new
+$loader.push_dir("src")
+$loader.enable_reloading
+$loader.setup
+
+def reload!
+  $loader.reload
+end
