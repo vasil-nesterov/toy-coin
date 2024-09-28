faedc693af5d60bc41b80060ed8f1db33a59c02e
Vasil Nesterov <vasil@hey.com>
2024-09-28 11:44:19 +0200

    ProofOfWork: toy implementation

---
+++ spec/proof_of_work_spec.rb
@@ -0,0 +1,33 @@
+
+require "rspec"
+require_relative "../src/proof_of_work"
+
+describe ProofOfWork do
+  let(:complexity) { 1 }
+  let(:pow) { ProofOfWork.new(complexity) }
+
+  describe ".digest" do
+    it "returns the digest of the proof and previous proof" do
+      expect(ProofOfWork.digest(35, 0)).to eq("0c3f78fba5399c309a2882d46cddbc8fca270bab448c680ee2561698d63ecd23")
+    end
+  end
+
+  describe "#next_proof" do
+    it "finds the next proof" do
+      previous_proof = 0
+      next_proof = pow.next_proof(previous_proof)
+      
+      expect(pow.valid_proof?(next_proof, previous_proof)).to be(true)
+    end
+  end
+
+  describe "#valid_proof?" do
+    it "validates a correct proof" do
+      expect(pow.valid_proof?(35, 0)).to be(true)
+    end
+
+    it "rejects an invalid proof" do
+      expect(pow.valid_proof?(0, 0)).to be(false)
+    end
+  end
+end
---
+++ src/proof_of_work.rb
@@ -0,0 +1,24 @@
+require "digest"
+
+class ProofOfWork
+  MAX_AVAILABLE_PROOF = 32.then { |n| 2**(n * 8) }
+
+  def self.digest(proof, previous_proof)
+    Digest::SHA256.hexdigest("#{proof}>#{previous_proof}")
+  end
+
+  def initialize(complexity)
+    @complexity = complexity
+  end
+
+  def next_proof(previous_proof)
+    loop do
+      possible_proof = rand(MAX_AVAILABLE_PROOF)
+      return possible_proof if valid_proof?(possible_proof, previous_proof)
+    end
+  end
+
+  def valid_proof?(proof, previous_proof)
+    self.class.digest(proof, previous_proof).start_with?("0" * @complexity)
+  end
+end
