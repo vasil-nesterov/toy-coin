3768d0296b8e71a9acf6e42d1cec8099d010ad73
Vasil Nesterov <vasil@hey.com>
2024-10-20 21:55:16 +0200

    Add Mempool spec

--- config/spec_helper.rb
+++ config/spec_helper.rb
@@ -1,7 +1,8 @@
 # typed: strict
 
 require 'debug'
-require 'logger'
+
+extend T::Sig
 
 ENV['RSPEC'] = 'true'
 
@@ -10,4 +11,13 @@ Log = Logger.new(STDOUT, level: Logger::WARN)
 RSpec.configure do |c|
   c.filter_run(focus: true)
   c.run_all_when_everything_filtered = true
+end
+
+sig { returns(Tx) }
+def simple_coinbase_tx
+  File
+    .read("#{ROOT_DIR}/spec/fixtures/simple_blockchain.json") 
+    .then { |json| JSON.parse(json) }
+    .then { |hash| hash.dig(0, "txs", 0) }
+    .then { |tx| TxSerializer.from_representation(tx) }
 end
--- spec/mempool_spec.rb
+++ spec/mempool_spec.rb
@@ -1,48 +1,23 @@
-# typed: false
-# 
-require 'rspec'
+# typed: strict
 
 RSpec.describe Mempool do
-  let(:alice) { Key.load_from_file("#{ROOT_DIR}/spec/fixtures/alice_test.key") }
+  T.bind(self, T.untyped)
 
-  let(:transaction) { 
-    Transaction.new(
-      sender: alice.address, 
-      recipient: 'Bob', 
-      value: 10.0
-    ).tap { _1.sign_with_key(alice) }
-  }
   let(:mempool) { Mempool.new }
 
-  describe '#add_transaction' do
-    it 'adds a transaction to the mempool' do
-      mempool.add_transaction(transaction)
-
-      expect(mempool.to_h).to include(transaction.to_h)
+  describe "#add_tx" do
+    it "adds a transaction to the mempool" do
+      expect {
+        mempool.add_tx(simple_coinbase_tx)
+      }.to change { mempool.to_representation.count }.by(1)
     end
   end
 
-  context 'with transaction' do
-    before do
-      mempool.add_transaction(transaction)
-    end
+  describe "#to_representation" do
+    let(:representation) { mempool.to_representation }
 
-    describe '#wipe' do
-      it 'returns the transactions and clears the mempool' do
-        result = mempool.wipe
-    
-        expect(result).to be_an(Array)
-        expect(result.count).to eq(1)
-        expect(result).to all(be_a(Transaction))
-
-        expect(mempool.to_h).to be_empty
-      end
-    end
-
-    describe '#to_h' do
-      it 'returns an array of transaction hashes' do
-        expect(mempool.to_h).to include(transaction.to_h)
-      end
+    it "returns an array of serialized txs" do
+      expect(representation).to eq([])
     end
   end
 end
