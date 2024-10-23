62ac442d975f8c76f682292aa60943b687f8bf47
Vasil Nesterov <vasil@hey.com>
2024-10-23 20:52:29 +0200

    typed: strict in middleware classes

--- config/middleware/node_middleware.rb
+++ config/middleware/node_middleware.rb
@@ -1,13 +1,15 @@
-# typed: true
+# typed: false
 
 class NodeMiddleware
   extend T::Sig
 
+  sig { params(app: Proc, node: Node).void }
   def initialize(app, node)
     @app = app
     @node = node
   end
 
+  sig { params(env: T::Hash[String, T.untyped]).returns(T.untyped) }
   def call(env)
     env['node'] = @node
     @app.call(env)
--- config/middleware/wallet_middleware.rb
+++ config/middleware/wallet_middleware.rb
@@ -1,13 +1,15 @@
-# typed: true
+# typed: strict
 
 class WalletMiddleware
   extend T::Sig
 
+  sig { params(app: Proc, wallet: Wallet).void }
   def initialize(app, wallet)
     @app = app
     @wallet = wallet
   end
 
+  sig { params(env: T::Hash[String, T.untyped]).returns(T.untyped) }
   def call(env)
     env['wallet'] = @wallet
     @app.call(env)
